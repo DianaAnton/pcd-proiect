1ebe8b0e20f873c8baa099958f628069e74b17ef
Diana Anton <diana-ana-maria.anton@atos.net>
2022-05-12 18:53:25 +0300

    Files part 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+// #include <iostream>
+
+// int main() {
+//     std::cout << "Hello Easy C++ project!" << std::endl;
+//      std::cout << "Hello Easy C++ project!" << std::endl;
+// }
 #include <iostream>
 
+#include "http_server.h"
+#include "served/multiplexer.hpp"
+
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
-     std::cout << "Hello Easy C++ project!" << std::endl;
+  served::multiplexer multiplexer;
+  learning::HttpServer http_server(multiplexer);
+
+  http_server.InitializeEndpoints();
+  http_server.StartServer();
+
+  return (EXIT_SUCCESS);
 }

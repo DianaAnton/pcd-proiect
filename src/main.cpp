5db3e752170041740037038c2c9f5fb7d2ab7c08
Diana Anton <diana.anton00@e-uvt.ro>
2022-05-13 15:33:06 +0300

    Cleaned junk

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
-// #include <iostream>
-
-// int main() {
-//     std::cout << "Hello Easy C++ project!" << std::endl;
-//      std::cout << "Hello Easy C++ project!" << std::endl;
-// }
 #include <iostream>
-
-#include "http_server.h"
-#include "served/multiplexer.hpp"
-
+using namespace std;
 int main() {
-  served::multiplexer multiplexer;
-  learning::HttpServer http_server(multiplexer);
-
-  http_server.InitializeEndpoints();
-  http_server.StartServer();
-
-  return (EXIT_SUCCESS);
+  return 0;
 }
